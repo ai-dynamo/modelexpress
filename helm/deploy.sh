c5dc5fe1aab8dfea2e1129dc982ef1218b383d99
Harrison Saturley-Hall <harrison.saturley.hall@gmail.com>
2025-10-07 12:16:16 -0400

    fix: add spdx headers to helm chart files (#94)
    Signed-off-by: Harrison Saturley-Hall <hsaturleyhal@nvidia.com>

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # ModelExpress Helm Chart Deployment Script
 # This script helps deploy ModelExpress using Helm
