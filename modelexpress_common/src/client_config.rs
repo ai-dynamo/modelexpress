6c7fd4e2d572a4ecbc60cfb23941d8c31424966a
KavinKrishnan <kavinkrishnan@gmail.com>
2026-01-22 15:10:05 -0800

    chore: update copyright year to 2025-2026 in all files (#129)
    Signed-off-by: Kavin Krishnan <kavink@nvidia.com>
    Signed-off-by: Harrison King Saturley-Hall <hsaturleyhal@nvidia.com>

--- modelexpress_common/src/client_config.rs
+++ modelexpress_common/src/client_config.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::cache::CacheConfig;
@@ -250,6 +250,7 @@ impl ClientConfig {
 }
 
 #[cfg(test)]
+#[allow(clippy::expect_used)]
 mod tests {
     use super::*;
     use crate::constants;
