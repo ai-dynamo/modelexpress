6e80d26c232d142c8b98e28c5f445fe7e7e13e6b
emricksini-h <emrick.birivoutin@hcompany.ai>
2026-05-04 13:23:57 +0200

    Changes following review
    Signed-off-by: emricksini-h <emrick.birivoutin@hcompany.ai>

--- modelexpress_client/python/modelexpress/tracing.py
+++ modelexpress_client/python/modelexpress/tracing.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 try:
     from opentelemetry import trace
     tracer = trace.get_tracer("modelexpress")
